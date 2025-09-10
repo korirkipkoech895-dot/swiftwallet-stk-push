67e6aa90cf809b1e6cbc2572281a68c0c73b005f
korirkipkoech895-dot <korirkipkoech895@gmail.com>
2025-09-10 13:40:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://swift-loan.onrender.com'
+  origin: 'https://swift-portal.onrender.com'
 }));
 
 function formatPhone(phone) {
